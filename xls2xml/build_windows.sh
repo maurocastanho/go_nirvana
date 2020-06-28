bbea427aeca75f71342813205e7d88ca194795e8
Mauro Castanho <castanhomauro@gmail.com>
2020-06-28 01:01:12 -0300

    xls2xml - build and zip script

--- xls2xml/build_windows.sh
+++ xls2xml/build_windows.sh
@@ -1,1 +1,5 @@
-GOPATH=/home/mauro/go/windows GOOS=windows GOARCH=amd64 go build
+#!/bin/bash
+fname=xls2xml_$(/bin/date +%Y%m%d).zip
+rm ${fname}
+GOPATH=/home/mauro/go/windows GOOS=windows GOARCH=amd64 go build && \
+zip ${fname} xls2xml.exe config*.json
